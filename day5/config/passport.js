3f8cb6aa01db7c46052a8413f0279438855ebdfd
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2016-06-30 15:40:12 -0600

    3:40pm

--- day5/config/passport.js
+++ day5/config/passport.js
@@ -1,9 +1,16 @@
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy
+var User = require('../user/model')
 
 passport.use(new LocalStrategy(
-	function (username, password, next)
+	function (email, password, next)
 	{
-
+		User.findByEmail(email, function (err, user)
+		{
+			if (err) return next(err)
+			if (!user) return next(null, false)
+			if (!user.verifyPassword(password)) return next(null, false)
+			return next(null, user)
+		})
 	}
 ))
